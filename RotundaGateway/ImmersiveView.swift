17e4ed381215622348316bca3b7de91a2d96703a
Riana-S <rianasantos808@gmail.com>
2025-04-29 00:55:45 -0700

    Window frame created

--- RotundaGateway/ImmersiveView.swift
+++ RotundaGateway/ImmersiveView.swift
@@ -10,11 +10,21 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
+    
+    @State private var door = Entity()
+    
     var body: some View {
         RealityView { content in
             // Add the initial RealityKit content
-            if let scene = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+            if let scene = try? await Entity(named: "RGScene", in: realityKitContentBundle) {
                 content.add(scene)
+                
+                guard let door = scene.findEntity(named: "Window") else{
+                    fatalError()
+                }
+                self.door = door
+                door.position = [0, 1, -2]
+                door.scale *= [1, 2, 1]
             }
         }
     }
